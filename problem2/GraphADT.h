6d5ac4847ebb5e3cc3de55ab1e57bf9f1be936a1
RaymondSWE <RamanMohammed@hotmail.com>
2022-12-01 10:20:23 +0100

    pushing in order to work on it for lab time

---
+++ problem2/GraphADT.h
@@ -0,0 +1,28 @@
+
+#include <iostream>
+#include <list>
+using namespace std;
+/* Graph class representing a
+directed graph using
+adjacency lists representation */
+class Graph
+{
+private:
+    int V; // No. of vertices
+    list<int> *adj;
+
+    /* Pointer to an
+    array containing
+    adjacency lists */
+public:
+    Graph(int V); // Constructor
+    /* Add an edge to graph */
+    void addEdge(int v, int w);
+    // check if there is an  edge in graph
+    bool isEdge(int v, int w);
+    // get number of nodes
+    int getNumNodes();
+    /* print the adjacency list
+    representation of graph*/
+    void printGraph();
+};
---
+++ problem2/TimeComplexity.c++
@@ -0,0 +1,4 @@
+// Create a time complexity analysis with O(-) notation
+// For BFS, the time complexity is O(V + E) where V is the number of vertices in the graph and E is the number of edges in the graph.
+// For DFS, the time complexity is O(V + E) where V is the number of vertices in the graph and E is the number of edges in the graph.
+// For Dijkstra’s algorithm, the time complexity is O(V2) where V is the number of vertices in the graph.
