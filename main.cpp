9e61bed3bb4a1c94e341b16d2750ddc032108303
nibarsito <nibarsito2@gmail.com>
2022-12-08 21:35:55 +0100

    made some changes to the code.

--- main.cpp
+++ main.cpp
@@ -9,14 +9,17 @@ Task 4.1 Design a trivial(the most obvious solution) recursive algorithm that
 calculates T(n) in the expression(2).Implement the algorithm in C / C ++
 */
 int calculateTC(int n) {
-	if(n==1)
-	return n;
-	return  calculateTC(n-1)+calculateTC(ceil(n/2))+n;
-
+	int currentValue {} (n-1) + ceil(n / 2) + n};
+	cout << currentValue << endl; 
+	
+	if (static_cast<int>(currentValue)==1) {
+		return n;
+	}
+	return calculateTC(currentValue);
 }
 
 int main() {
-	cout << calculateTC(3) << '\n';
+calculateTC(1);
 return 0;
 }
 
