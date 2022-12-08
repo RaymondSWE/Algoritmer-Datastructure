4ae15be556ca3e6592be29bddc4d48dbaa4cc0a0
a21jeaha <a21jeaha@student.his.se>
2022-12-08 11:34:53 +0100

    added calculateTC function

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 
-// T (n) = T (n - 1) + T (dn/2e) + n
-// T (1) = 1
+// T(n) = T(n - 1) + T(n/2) + n
+// T(1) = 1
 // 
-// where dn/2e is the least integer greater than n/2.
+// where n/2 is the least integer greater than n/2.
 // 
 //Task 4.1 Design a trivial(the most obvious solution) recursive algorithm that
 //calculates T(n) in the expression(2).Implement the algorithm in C / C ++
@@ -15,6 +15,11 @@ int main() {
 
 }
 
-int calculateTC() {
-	return calculateTC(), calculateTC();
+int calculateTC(int n) {
+	if (n == 1) {
+		return n;
+	}
+	int currentValue = (n-1) + ceil(n / 2) + n; 
+	cout << currentValue << endl; 
+	return calculateTC(currentValue);
 };
