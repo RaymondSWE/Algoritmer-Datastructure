325b81af08ab5562911058482a6b9b93a7c6bab6
nibarsito <61316519+nibarsito@users.noreply.github.com>
2022-12-09 10:25:21 +0100

    fixed an issue with ciel that gave the wrong values

--- problem4/main.cpp
+++ problem4/main.cpp
@@ -1,22 +1,21 @@
+#include <chrono>
 #include <iostream>
+using namespace std::chrono;
 using namespace std;
-/*
- T(n) = T(n - 1) + T(n/2) + n
- T(1) = 1
- where n/2 is the least integer greater than n/2.
-
-Task 4.1 Design a trivial(the most obvious solution) recursive algorithm that
-calculates T(n) in the expression(2).Implement the algorithm in C / C ++
-*/
-int calculateTC(int n) {
-	if(n==1)
-	return n;
-	return  calculateTC(n-1)+calculateTC(ceil(n/2))+n;
-
-}
-
+int calculateTC(int n) {    
+    if (n == 1) {
+        return n;
+    }
+    int cielValue = (n / 2) + ((n % 2) != 0);
+    int currentValue = calculateTC(n - 1) + calculateTC(cielValue) + n;
+    
+    return currentValue;
+};
 int main() {
-	cout << calculateTC(3) << '\n';
-return 0;
+    auto start = high_resolution_clock::now();
+    cout << calculateTC(3) << endl;
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop-start);
+    cout << duration.count() << " milliseconds" << endl;
+    return 0;
 }
-
