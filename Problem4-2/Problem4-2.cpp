09d9fa96cc539d82a5c6feab3f889739a7d0562c
a21jeaha <a21jeaha@student.his.se>
2022-12-10 13:39:21 +0100

    commit

---
+++ Problem4-2/Problem4-2.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <chrono>
+#include <vector>
+using namespace std;
+using namespace std::chrono;
+
+// T(n) = T(n - 1) + T(n/2) + n             //n + T(n-1) + T(n/2)????
+// T(1) = 1
+// 
+// where n/2 is the least integer greater than n/2.
+// 
+//Task 4.1 Design a trivial(the most obvious solution) recursive algorithm that
+//calculates T(n) in the expression(2).Implement the algorithm in C / C ++
+//code.
+int calculateTC(int n);
+int topDownTC(int n, vector<vector<int>>& resultVector);
+void pushBackFunction(int n, vector<vector<int>>& resultVector);
+
+int main() {
+	vector<vector<int>> resultVector;
+
+	auto start = high_resolution_clock::now();
+
+	cout << calculateTC(50) << endl;
+	cout << topDownTC(50, resultVector) << endl;
+
+	auto stop = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop - start);
+
+	cout << duration.count() << " milliseconds" << endl;
+	return 0;
+}
+
+int calculateTC(int n) {
+	if (n == 1) {
+		return n;
+	}
+	int cielValue = (n / 2) + ((n % 2) != 0);
+	int currentValue = calculateTC(n - 1) + calculateTC(cielValue) + n;
+
+	return currentValue;
+};
+
+void pushBackFunction(int n, vector<vector<int>>& resultVector) {
+	int tempValue;
+	int cielValue = (n / 2) + ((n % 2) != 0);
+	tempValue = topDownTC((n - 1), resultVector) + topDownTC(cielValue, resultVector) + n;
+	resultVector.push_back({ n , tempValue });
+}
+
+int topDownTC(int n, vector<vector<int>>& resultVector) {
+	if (n == 1) {
+		return n;
+	}
+	else {
+
+		int result;
+
+		if (resultVector.size() == 0) {
+
+			pushBackFunction(n, resultVector);
+		}
+
+		for (int i = 0; i < resultVector.size(); i++) {
+			if (resultVector[i][0] == n) {
+				result = resultVector[i][1];
+			}
+		}
+
+		if (result == NULL) {
+
+			pushBackFunction(n, resultVector);
+		}
+
+		return result;
+	}
+};
