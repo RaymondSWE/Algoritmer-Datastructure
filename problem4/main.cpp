f04df6d874cee9d4b5e2d006fe7d818780df273e
nibarsito <nibarsito2@gmail.com>
2022-12-10 01:49:50 +0100

    added a more elegant solution and changed out the vector for a 1d array.

--- problem4/main.cpp
+++ problem4/main.cpp
@@ -1,39 +1,23 @@
 #include <chrono>
 #include <vector>
+#include <algorithm>
 #include <iostream>
 using namespace std::chrono;
 using namespace std;
-int topDown(int n, vector<vector<int>> &result)
+int topDown(int n, int (&result)[15])
 {
-    int cielValue; 
-    int currentValue; 
+    int cielValue=0; 
+    int currentValue=0; 
 
-if(n==1)
-return n;
-if(result[0][0]==0)
-{
-    cout << "empty\n";
-cielValue= (n / 2) + ((n % 2) != 0);
-	currentValue= topDown(n - 1, result) + topDown(cielValue, result) + n;
-//    result.push_back({0});
-result.at(0).at(0)= n;
-cout << result[0][0] << '\n';
-result.at(0).at(1)= currentValue;
-}
+if(result[n]!=-1)
+return result[n];
 
-for(int i=0; i<result.size(); ++i)
-{
-	if(result[i][0]!=n)
+if(n>0)
 {
 cielValue= (n / 2) + ((n % 2) != 0);
 	currentValue= topDown(n - 1, result) + topDown(cielValue, result) + n;
-result[i].push_back(n);
-result[i].push_back(currentValue);
-
 }
-
-}
-
+result[n]=currentValue;
 return currentValue;
 }
 
@@ -47,15 +31,10 @@ int calculateTC(int n) {
     return currentValue;
 };
 int main() {
-	vector<vector<int> > result;
-    result.resize(3, vector<int>(3, 0));
-//    vector<vector<int> >    result(8, vector<int>(3));
-		cout << topDown(3, result);
+int result[15];
+std::fill_n(result, 15, -1);
+    	cout << topDown(3, result) << '\n';
 
-for(int i=0; i<result.size(); ++i)
-{
-cout << result[i][1];
-}
 /*
     auto start = high_resolution_clock::now();
     cout << calculateTC(3) << endl;
