5fc1bcddcba505f8e143670fe096717c6a7621ab
a21jeaha <a21jeaha@student.his.se>
2022-12-10 13:44:11 +0100

    comments added

--- Problem4-2/Problem4-2.cpp
+++ Problem4-2/Problem4-2.cpp
@@ -12,14 +12,15 @@ using namespace std::chrono;
 //Task 4.1 Design a trivial(the most obvious solution) recursive algorithm that
 //calculates T(n) in the expression(2).Implement the algorithm in C / C ++
 //code.
-int calculateTC(int n);
-int topDownTC(int n, vector<vector<int>>& resultVector);
+
+int calculateTC(int n);											// Trivial version 
+int topDownTC(int n, vector<vector<int>>& resultVector);		// dynamic programing version
 void pushBackFunction(int n, vector<vector<int>>& resultVector);
 
 int main() {
-	vector<vector<int>> resultVector;
+	vector<vector<int>> resultVector;							// 2D-vector to store n:values and equation results
 
-	auto start = high_resolution_clock::now();
+	auto start = high_resolution_clock::now();					// timer
 
 	cout << calculateTC(50) << endl;
 	cout << topDownTC(50, resultVector) << endl;
@@ -31,6 +32,7 @@ int main() {
 	return 0;
 }
 
+// The trivial solution for problem 4
 int calculateTC(int n) {
 	if (n == 1) {
 		return n;
@@ -41,6 +43,7 @@ int calculateTC(int n) {
 	return currentValue;
 };
 
+// Pushes values into the back of the vector 
 void pushBackFunction(int n, vector<vector<int>>& resultVector) {
 	int tempValue;
 	int cielValue = (n / 2) + ((n % 2) != 0);
@@ -48,6 +51,7 @@ void pushBackFunction(int n, vector<vector<int>>& resultVector) {
 	resultVector.push_back({ n , tempValue });
 }
 
+// solves problem 4 with dynamic proraming 
 int topDownTC(int n, vector<vector<int>>& resultVector) {
 	if (n == 1) {
 		return n;
